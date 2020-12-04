38bc0ecdf8e540b94feb2a92ecabce11d0ff9aa7
coda1997 <overlooker1997@gmail.com>
2020-12-04 17:01:49 +0800

    tempo saved

--- app/src/main/java/com/dadachen/isitp/IMUCollector.kt
+++ app/src/main/java/com/dadachen/isitp/IMUCollector.kt
@@ -1,16 +1,31 @@
 package com.dadachen.isitp
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.util.Log
 
 class IMUCollector(val context: Context) {
-    val data = Array<FloatArray>(6){
+    private val gyro = FloatArray(3)
+    private val acc = FloatArray(3)
+    private val rotVector = FloatArray(4)
+    private lateinit var sensorManager: SensorManager
+    private var rotVSensor: Sensor? = null
+    private var accVSensor: Sensor? = null
+    private var gyroVSensor: Sensor? = null
+    val data = Array(6){
         FloatArray(200)
     }
     fun start(){
+        initSensor()
         isRunning = true
     }
     fun stop(){
         isRunning = false
+        stopSensor()
     }
     val lowpassFilters = Array<IMULowPassFilter>(6){
         IMULowPassFilter(FilterConstant.para)
@@ -30,4 +45,54 @@ class IMUCollector(val context: Context) {
 
     var isRunning = false
 
+    private val rotl = object : SensorEventListener {
+        @SuppressLint("SetTextI18n")
+        override fun onSensorChanged(p0: SensorEvent?) {
+            rotVector[0] = p0!!.values[0]
+            rotVector[1] = p0.values[1]
+            rotVector[2] = p0.values[2]
+            rotVector[3] = p0.values[3]
+
+        }
+
+        override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+            Log.d("imu", "rot accuracy changed")
+        }
+    }
+    private val gyrol = object : SensorEventListener {
+        override fun onSensorChanged(p0: SensorEvent?) {
+            gyro[0] = p0!!.values[0]
+            gyro[1] = p0.values[1]
+            gyro[2] = p0.values[2]
+        }
+
+        override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+            Log.d("imu", "gyro accuracy changed")
+        }
+    }
+    private val accl = object : SensorEventListener {
+        override fun onSensorChanged(p0: SensorEvent?) {
+            acc[0] = p0!!.values[0]
+            acc[1] = p0.values[1]
+            acc[2] = p0.values[2]
+        }
+
+        override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+            Log.d("imu", "acc accuracy changed")
+        }
+    }
+    private fun initSensor() {
+        sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        rotVSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GAME_ROTATION_VECTOR)
+        accVSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        gyroVSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE_UNCALIBRATED)
+        sensorManager.registerListener(rotl, rotVSensor, SensorManager.SENSOR_DELAY_FASTEST)
+        sensorManager.registerListener(accl, accVSensor, SensorManager.SENSOR_DELAY_FASTEST)
+        sensorManager.registerListener(gyrol, gyroVSensor, SensorManager.SENSOR_DELAY_FASTEST)
+    }
+    private fun stopSensor() {
+        sensorManager.unregisterListener(accl)
+        sensorManager.unregisterListener(gyrol)
+        sensorManager.unregisterListener(rotl)
+    }
 }
