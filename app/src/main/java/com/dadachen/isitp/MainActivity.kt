9872d446b1a75a5631dbeb2753b18a600a1c266b
coda1997 <overlooker1997@gmail.com>
2020-12-05 11:08:48 +0800

    load modules

--- app/src/main/java/com/dadachen/isitp/MainActivity.kt
+++ app/src/main/java/com/dadachen/isitp/MainActivity.kt
@@ -8,20 +8,21 @@ import kotlinx.android.synthetic.main.activity_main.*
 import org.pytorch.IValue
 import org.pytorch.Module
 import org.pytorch.Tensor
-
+/* *
+* 1st, obtain IMU data,done
+* 2nd, post processing IMU data by low-pass filters. todo
+* 3rd, transfer it to tensors done
+* 4th, forward the pre-saved net with tensors done
+* 5th, calculate the estimated location and display it on UI. todo
+* 6th, additionally, it supports gesture estimation and show the doubt value. todo
+* 7th, remember, it should be always fully tested with both unit and integrated. todo
+* @Author dadachen
+* */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //1st, obtain IMU data,
-        //2nd, post processing IMU data by low-pass filters. done
-        //3rd, transfer it to tensors done
-        //4th, forward the pre-saved net with tensors done
-        //5th, calculate the estimated location and display it on UI
-        //6th, additionally, it supports gesture estimation and show the doubt value
-        //7th, remember, it should be always fully tested with both unit and integrated.
-        //transfer post-processed IMU data to tensors
-        //TODO(implement the path by Uri or etc)
+
         initView()
     }
 
